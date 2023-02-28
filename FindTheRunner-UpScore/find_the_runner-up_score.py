25d41885f614402e8729ef85208c4530f96126b0
SamuelCruzGz <samuel.sebas46@gmail.com>
2023-02-28 10:24:47 -0600

    Solution to Problem

--- FindTheRunner-UpScore/find_the_runner-up_score.py
+++ FindTheRunner-UpScore/find_the_runner-up_score.py
@@ -1,1 +1,8 @@
-print('hello world')
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arrSorted = sorted(arr, reverse= True)
+    for i in range(len(arrSorted)):
+        if arrSorted[i]!= arrSorted[0]:
+            print(arrSorted[i])
+            break
